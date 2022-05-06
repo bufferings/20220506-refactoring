15c3fb66310dbae1e381d36c1bb2086e2f49498e
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-05-06 16:26:54 +0900

    Initial code

---
+++ src/main/java/demo/Product.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Product {
+  final int id;
+  final String name;
+  final int price;
+
+  public Product(int id, String name, int price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+}
