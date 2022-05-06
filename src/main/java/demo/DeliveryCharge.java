b0bf4c6cd7718dc240a55b65cb71ff95e66602e9
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-05-06 17:45:27 +0900

    Refactor DeliveryCharge logic

--- src/main/java/demo/DeliveryCharge.java
+++ src/main/java/demo/DeliveryCharge.java
@@ -1,16 +1,18 @@
 package demo;
 
 public class DeliveryCharge {
+  private static final int CHARGE_FREE_THRESHOLD = 2000;
+  private static final int PAY_CHARGE = 500;
+  private static final int CHARGE_FREE = 0;
+
   final int amount;
 
   public DeliveryCharge(ShoppingCart cart) {
-    int charge = 0;
     int totalPrice = cart.totalPrice();
-    if (totalPrice < 2000) {
-      charge = 500;
+    if (totalPrice < CHARGE_FREE_THRESHOLD) {
+      amount = PAY_CHARGE;
     } else {
-      charge = 0;
+      amount = CHARGE_FREE;
     }
-    this.amount = charge;
   }
 }
