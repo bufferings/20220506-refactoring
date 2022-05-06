ecf4e3d40ceb592704803d44002f978fa5008229
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-05-06 17:30:57 +0900

    Refactor test

--- src/test/java/demo/DeliveryChargeTest.java
+++ src/test/java/demo/DeliveryChargeTest.java
@@ -2,22 +2,14 @@ package demo;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DeliveryChargeTest {
   @Test
   public void 商品の合計金額が2000円未満の場合_配送料は500円() {
-    var products = List.of(
-        new Product(1, "商品A", 500),
-        new Product(2, "商品B", 1499)
-    );
-
     var cart = new ShoppingCart();
-    for (var elem : products) {
-      cart = cart.add(elem);
-    }
+    cart = cart.add(new Product(1, "商品A", 500));
+    cart = cart.add(new Product(2, "商品B", 1499));
     var charge = new DeliveryCharge(cart);
 
     assertEquals(500, charge.amount);
@@ -25,15 +17,9 @@ class DeliveryChargeTest {
 
   @Test
   public void 商品の合計金額が2000円以上の場合_配送料は無料() {
-    var products = List.of(
-        new Product(1, "商品A", 500),
-        new Product(2, "商品B", 1500)
-    );
-
     var cart = new ShoppingCart();
-    for (var elem : products) {
-      cart = cart.add(elem);
-    }
+    cart = cart.add(new Product(1, "商品A", 500));
+    cart = cart.add(new Product(2, "商品B", 1500));
     var charge = new DeliveryCharge(cart);
 
     assertEquals(0, charge.amount);
